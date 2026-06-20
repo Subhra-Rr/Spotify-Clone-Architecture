af7bef36c06395b79aeab793e466bab14a44f579
Subhradeet Sabat <sdsabat2006@gmail.com>
2026-06-20 13:00:30 +0530

    chore: remove Capacitor dependency
    Remove Capacitor from dependencies and delete the associated Android native directory as the project transitions away from hybrid mobile development.

--- android/app/src/main/java/com/music/spotifyclone/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.music.spotifyclone;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.music.spotifyclone',
-  appName: 'Spotify Clone',
-  webDir: 'dist'
-};
-
-export default config;
